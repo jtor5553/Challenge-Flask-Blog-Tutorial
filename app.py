253790611414d9b580b7ca4904575116d91ae8ab
Jose Torres-Gomez <jose.torres-gomez@syncrostore.com>
2025-11-05 21:12:31 -0600

    added create section

---
+++ app.py
@@ -0,0 +1,59 @@
+import sqlite3
+from flask import Flask, render_template, request, url_for, flash, redirect, abort
+
+# make a Flask application object called app
+app = Flask(__name__)
+app.config["DEBUG"] = True
+app.config['SECRET_KEY'] = 'your secret key'
+
+
+# Function to open a connection to the database.db file
+def get_db_connection():
+    # create connection to the database
+    conn = sqlite3.connect('database.db')
+    
+    # allows us to have name-based access to columns
+    # the database connection will return rows we can access like regular Python dictionaries
+    conn.row_factory = sqlite3.Row
+
+    #return the connection object
+    return conn
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required')
+        elif not content:
+            flash('Content is required')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)', (title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('create.html')
+
+# use the app.route() decorator to create a Flask view function called index()
+@app.route('/')
+def index():
+    #get connection to the database
+    conn = get_db_connection()
+
+    #execute a query to read all posts from the "posts" table in db
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+
+    #close the database connection
+    conn.close()
+
+    #send the posts to the index.html template and return the rendered HTML
+    return render_template('index.html', posts=posts)
+
+
+# route to create a post
+
+
+app.run(port=5000)
